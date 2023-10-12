40d302ee351006fd425f0e6527cd235848f1a74a
Diego <rijmjada@gmail.com>
2023-10-12 09:48:26 -0300

    first

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', ()=> {
+    window.open('https://chrome.google.com/webstore/detail/highlighter-pdf/fefpgdllnapgapghnmhofohgldmfabia', '_blank');
+});
